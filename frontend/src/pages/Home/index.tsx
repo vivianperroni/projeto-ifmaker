64f3f20485bc325f2f58738c58c56432ef661be1
vivianperroni <vivian.perroni@hotmail.com>
2021-09-13 01:11:31 -0400

    Fixando footer e melhorando detalhes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -9,9 +9,11 @@ const Home = () => {
             <div className="container">
                 <div className="jumbotron">
                     <h1 className="display-4">IFMaker</h1>
-                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <p className="lead">Agende horários disponíveis para os grupos que precisam utilizar as 
+                    ferramentas do espaço do IFMaker</p>
                     <hr />
-                    <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+                    <p>O IFMaker é um espaço para desenvolver a criatividade, curiosidade e proativadade de alunos
+                        que desejam criar produtos de boa qualidade.</p>
                     <Link className="btn btn-primary btn-lg" to="/calendar">Agendar</Link>
                 </div>
             </div>
